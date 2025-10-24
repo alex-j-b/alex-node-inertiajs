41310545d1dfa57f85b5361e9734b88bc64ed96c
Alex <alexis.j.boix@gmail.com>
2025-10-24 19:47:25 +0300

    Removed express session and flash

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 export { Inertia } from "./inertia.js";
 export { defineConfig } from "./define_config.js";
-export { default as Flash } from "./flash.js";
 export { resolvePageComponent } from "./helpers.js";
 export { InertiaHeaders } from "./headers.js";
 export * from "./props.js";
 export * from "./types.js";
-
-import "./types/express.js";
-import "./types/express-session.js";
